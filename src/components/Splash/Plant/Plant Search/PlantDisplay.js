c154d244d47a7338b9c64261bac9f26a92d9a8e3
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-12-17 07:35:46 -0500

    Plant display not working yet, groundwork laid for fetchMorePlant

--- src/components/Splash/Plant/Plant Search/PlantDisplay.js
+++ src/components/Splash/Plant/Plant Search/PlantDisplay.js
@@ -1,8 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { CardGroup, Col, Card, CardImg, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+
  const PlantDisplay = (props) => {
+    //  const [onePlant, setOnePlant] = useState(false);
+    //  const [grabPlant, setGrabPlant] = useState({});
+
      return(
          <div className="Plants">
-
+           <CardGroup>
+               {props.plant.results.map((plant, index) => {
+                   return (
+                       <Col xs = '12' sm = '10' md = '6' lg = '4' key={index}>
+                           <Card id={results.id}/>
+                               <CardImg alt='plant image placeholder'
+                               src={props.plant.results.imageurl}
+                               top
+                               width='100%'/>
+                               <CardTitle tag= 'h5'>{props.plant.results.tvfname}</CardTitle>
+                               <CardSubtitle>{props.plant.results.othname}</CardSubtitle>
+                               <CardBody>
+                                   <CardText>For more information click the card title.</CardText>
+                               </CardBody>
+                           </Card>
+                       </Col>
+                    )
+                </CardGroup>
+            </div>
+               })}
+                 <li>{props.plant.results.imageurl}</li> {/*will be the imagecap for the card*/}
+                 <li>{props.plant.results.tvfname}</li> {/*PRIMARY TITLE: Also, Needs to be a link that will fire the fetchMorePlant. fetchMorePlant will use tvfname here to run the tvfitem fetch so that we can get more data about each plant */}
+                 <li>{props.plant.results.othname}</li> {/*SUBTITLE */}
+            </CardGroup>     
          </div>
      )
  }
--- src/components/Splash/Plant/Plant.js
+++ src/components/Splash/Plant/Plant.js
@@ -7,7 +7,11 @@ const Plant = () => {
     const [plant, setPlant] = useState([]);
     const baseURL = "https://cors-anywhere.herokuapp.com/tropicalfruitandveg.com/api/tfvjsonapi.php?search=";
     const [search, setSearch] = useState('');
-    
+
+    //increased functionality
+    const moreURL = "https://cors-anywhere.herokuapp.com/tropicalfruitandveg.com/api/tfvjsonapi.php?tfvitem=";
+    const [clicked, setClicked] = useState('');
+    const [morePlant, setMorePlant] = useState('');
 
     const fetchPlant = () => {
 
@@ -26,13 +30,30 @@ const Plant = () => {
             });
     }
 
+    //Fetch for increased functionality.
+    /*need to set this as an onclick function once we set plant.results.tvfname as a link. This will enable customer to click on the name of the item they had actually intended in their search and get more information.*/
+    const fetchMorePlant = () => {
+        fetch((`${moreURL} + ${clicked}`), {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+            })
+        })
+            .then((res) => res.json())
+            .then((moreData) => {
+                setMorePlant(moreData);
+                console.log(moreData)
+            });
+    };
     useEffect(() => {
         fetchPlant();
+        fetchMorePlant();
     }, [])
     return (
     <div>
         <PlantSearch fetchPlant={fetchPlant} search ={search} setSearch={setSearch}/>
-        <PlantDisplay />
+        
+        <PlantDisplay clicked={clicked} plant={plant} morePlant={morePlant} setClicked={setClicked} fetchMorePlant={fetchMorePlant}/>
     </div>
         
     )
