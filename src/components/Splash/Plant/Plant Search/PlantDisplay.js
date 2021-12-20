a458565f9d4cdec80b0dad7cf2ae75c0289112e4
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-12-20 04:51:55 -0500

    adjusting styling

--- src/components/Splash/Plant/Plant Search/PlantDisplay.js
+++ src/components/Splash/Plant/Plant Search/PlantDisplay.js
@@ -1,19 +1,20 @@
 import React  from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 import './PlantDisplay.css';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+
 
 const PlantDisplay = (props) => {
 
     return (
-        <div className="Plants">
-             <Card className="plantCard" key="id">
-                <CardImg top width="100%" src={props.plant.imageurl} alt="Card image cap" />
+        <div className="plants">
+             <Card className="plantCard" >
+                <CardImg top width="100%" src= {props.plant.imageurl} alt="Card image cap" />
                     <CardBody>
-                    <CardTitle>Database Name: {props.plant.tfvname}</CardTitle>
-                    <CardSubtitle>Botanical Name: {props.plant.botname}</CardSubtitle>
+                        <CardTitle>Database Name: {props.plant.tfvname}</CardTitle>
+                        <CardSubtitle>Botanical Name: {props.plant.botname}</CardSubtitle>
                         <CardText>Also known as: {props.plant.othname}.</CardText>
-                        </CardBody>
-                    </Card> 
+                    </CardBody>
+            </Card> 
         </div>
 
     )
