b71a957276cb8e4fadcc4d23cb0eae20516dbbd4
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-12-20 05:57:09 -0500

    .more styling updates, now with borders! Still with no images or animations :( will allow you to search AND enter notes while having search results up. Does make a card for each entry, displaying pertinent information.

--- src/components/Splash/Plant/Plant Search/PlantDisplay.js
+++ src/components/Splash/Plant/Plant Search/PlantDisplay.js
@@ -8,7 +8,7 @@ const PlantDisplay = (props) => {
     return (
         <div className="plants">
              <Card className="plantCard" >
-                <CardImg top width="100%" src= {props.plant.imageurl} alt="Card image cap" />
+                <CardImg className="Image" top width="100%" src= {props.plant.imageurl} alt="Card image cap" />
                     <CardBody>
                         <CardTitle>Database Name: {props.plant.tfvname}</CardTitle>
                         <CardSubtitle>Botanical Name: {props.plant.botname}</CardSubtitle>
