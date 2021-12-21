04b0bbc63505c9d9ce379c7f0ed26ea0ccf68fb1
Danielle Joy <daniellejoy@danielles-mbp.lan>
2021-12-21 18:57:15 -0500

    fixed

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,14 +1,13 @@
-let APIURL = '',
+let APIURL = '';
 
 switch (window.location.hostname) {
-    case 'localhost' || '192.168.86.1' :
+    case 'localhost' || '127.0.0.1' :
         APIURL = 'http:localhost:3000';
         
         break;
 
     case 'midwest-tropicals.herokuapp.com/ ' :
-        APIURL= 'https://mwt-server.herokuapp.com/'
-        break;
+        APIURL= 'https://mwt-server.herokuapp.com'
 }
 
 export default APIURL;
